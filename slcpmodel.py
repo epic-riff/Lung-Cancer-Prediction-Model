bbf6bec6e6aa83b454a3686493a9b317d3f1daf3
epic-riff <lakshpatel510@gmail.com>
2025-07-22 01:14:03 -0400

    Update slcpmodel.py

--- slcpmodel.py
+++ slcpmodel.py
@@ -1,42 +1,55 @@
 # Import dependencies
-import pandas as pd
 import os
+# Format data
+import pandas as pd
+# Create Model Architecture 
+import torch
 from torch import nn, save, load
 from torch.optim import Adam
+# Load Dataset
 from torch.utils.data import DataLoader, Dataset
 from torchvision import datasets, transforms
+# Transform the Images for Training
 from torchvision.transforms import ToTensor, Compose, Resize
+# Access and Process Images
 from PIL import Image
-import torch
 from skimage import io
 
-# Prepare dataset
+# Prepare the custom dataset
 class LungCancerDataset(Dataset):
     def __init__(self, csv_file, root_dir, transform=None):
+        # Turn the csv file into a DataFrame
         self.annotations = pd.read_csv(csv_file)
+
+        # Other Settings
         self.root_dir = root_dir
         self.transform = transform
-        
+
+    # Finds the length of the dataset
     def __len__(self):
         return len(self.annotations)
-    
+        
+    # When an image is being accessed
     def __getitem__(self, index):
+        # Get Image, and match it with it's Y label
         img_path = os.path.join(self.root_dir, self.annotations.iloc[index, 0])
         image = io.imread(img_path)
         y_label = torch.tensor(int(self.annotations.iloc[index, 1]))
-        
+
+        # Apply Transformations
         if self.transform:
             image = self.transform(image)
-            
+
+        # Return the image and y label
         return (image, y_label)
     
 # Load Data
-dataset = LungCancerDataset(csv_file='C:\\Users\\Laksh-Games\\OneDrive\\Desktop\\Coding Files\\Py Stuff\\DL\\Lung Cancer Prediction\\lcdataset.csv', root_dir='C:\\Users\\Laksh-Games\\OneDrive\\Desktop\\Coding Files\\Py Stuff\\DL\\Lung Cancer Prediction\\data', transform = transforms.ToTensor())
+dataset = LungCancerDataset(csv_file='lcdataset.csv', root_dir='data', transform = transforms.ToTensor())
 train_set, test_set = torch.utils.data.random_split(dataset, [12500, 2499])
 train_loader = DataLoader(dataset=train_set, batch_size=32, shuffle=True)
 test_loader = DataLoader(dataset=test_set, batch_size=32, shuffle=True)
     
-# Create nn
+# Create Neural Network Architecture
 class LCModel(nn.Module):
     def __init__(self):
         # Image size is 768 x 768
@@ -44,11 +57,14 @@ class LCModel(nn.Module):
         self.model = nn.Sequential(
             nn.Conv2d(3, 32, (5,5)),
             nn.ReLU(),
-            nn.MaxPool2d(2, stride=2),          
+            nn.MaxPool2d(2, stride=2),
+            
             nn.Conv2d(32, 64, (5,5)),
             nn.ReLU(),
-            nn.MaxPool2d(2, stride=2),            
+            nn.MaxPool2d(2, stride=2),
+            
             nn.Flatten(),
+            
             nn.Linear(64*189*189, 128),
             nn.ReLU(),
             nn.Linear(128, 3)
@@ -66,11 +82,13 @@ loss_func = nn.CrossEntropyLoss()
 
 # Training Loop
 if __name__ == '__main__':
-    print('Starting training')
     for epoch in range(10): # Train for 10 epochs
         for batch in train_loader:
+            # Get batch
             X,y = batch
             X,y = X.to('cpu'), y.to('cpu')
+
+            # Feed the data to the model and calculate loss
             yhat = lcm(X)
             loss = loss_func(yhat, y)
             
@@ -78,7 +96,10 @@ if __name__ == '__main__':
             opt.zero_grad()
             loss.backward()
             opt.step()
+            
+        # Print when each epoch is complete    
         print(f"Epoch:{epoch} has a loss of {loss.item()}")
         
+# Save the model         
 with open('lcpmodel.pt', 'wb') as f:
     save(lcm.state_dict(), f)
